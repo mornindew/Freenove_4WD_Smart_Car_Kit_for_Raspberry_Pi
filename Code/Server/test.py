f2dd50f5573e0090e102e9b54812166b5bf911c3
mornindew <craigmarti@gmail.com>
2022-01-18 21:20:52 -0700

    removed the extra file

--- Code/Server/test.py
+++ Code/Server/test.py
@@ -19,30 +19,6 @@ def test_Led():
         led.colorWipe(led.strip, Color(0,0,0))  #turn off the light
         print ("\nEnd of program")
 
-        
-        
-# from Motor import *            
-# PWM=Motor()          
-# def test_Motor(): 
-#     try:
-#         PWM.setMotorModel(1000,1000,1000,1000)       #Forward
-#         print ("The car is moving forward")
-#         time.sleep(1)
-#         PWM.setMotorModel(-1000,-1000,-1000,-1000)   #Back
-#         print ("The car is going backwards")
-#         time.sleep(1)
-#         PWM.setMotorModel(-1500,-1500,2000,2000)       #Left 
-#         print ("The car is turning left")
-#         time.sleep(1)
-#         PWM.setMotorModel(2000,2000,-1500,-1500)       #Right 
-#         print ("The car is turning right")  
-#         time.sleep(1)
-#         PWM.setMotorModel(0,0,0,0)                   #Stop
-#         print ("\nEnd of program")
-#     except KeyboardInterrupt:
-#         PWM.setMotorModel(0,0,0,0)
-#         print ("\nEnd of program")
-
 from Motor import *            
 roverMotors=Motor() 
 roverLed=Led()         
